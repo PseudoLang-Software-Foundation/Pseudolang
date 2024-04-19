5faaf47046e4845a24ac134656466e5b21d5b27a
RobbyV2 <robby@robby.blue>
2024-04-19 10:55:14 -0500

    Changes

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,25 @@
+/*
+TODO:
+1. sometimes, unknown tokens cause the rest of the program to not be recognized, outputting []
+also make it so that unknown tokens are printed as themselves with Identifier
+EX: a < prints [] instead of [Identifier("a"), LessThan]
+< DISPLAY aaa prints [LessThan, DISPLAY] instead of [LessThan, DISPLAY, Identifier("aaa")]
+a < prints [] instead of [Identifier("a"), LessThan]
+IMPORTANT: If the token is not recognized, it should only be an identifier if before/after assignment, in a parameter, or in a list, this logic should be in the parser.
+
+2. Also, strings are not recognized, anything in double quotes should be a string
+
+3. Parameters DISPLAY("hi") should work by having tokens in between parentheses, so DISPLAY("hi") would be [DISPLAY(String("hi"))]
+Current it prints [Display, Unknown, Unknown, Identifier("hi"), Unknown, Unknown] instead of [Display(String("hi"))]
+
+4. List lexing: Ex: ["1", 1, 0.1, true] should be [ListCreate(String("1"), Integer(1), Float(0.1), Boolean(true))]
+
+5. Comment lexing: Ex: COMMENT asdasd should be [Comment], and anything after on the same line should be ignored.
+For comment blocks, remove everything in between the commentblock types.
+
+6. Raw strings r"asd", and multiline strings with """asd""" along with formatted strings f"asd {var}".
+*/
+
 #[derive(Debug, PartialEq)]
 #[allow(dead_code)]
 pub enum Token {
@@ -86,6 +108,33 @@ impl<'a> Lexer<'a> {
         }
     
         match next_char {
+            '"' => {
+                let mut string = String::new();
+                while let Some(next_char) = self.chars.clone().next() {
+                    if next_char != '"' {
+                        string.push(next_char);
+                        self.chars.next();
+                    } else {
+                        self.chars.next();
+                        break;
+                    }
+                }
+                Some(Token::String(string))
+            },
+            '(' => {
+                let mut params = Vec::new();
+                while let Some(next_char) = self.chars.clone().next() {
+                    if next_char != ')' {
+                        if let Some(token) = self.next_token() {
+                            params.push(token);
+                        }
+                    } else {
+                        self.chars.next();
+                        break;
+                    }
+                }
+                Some(Token::Parameters(params))
+            },
             '=' => Some(Token::Equal),
             '>' => Some(Token::GreaterThan),
             '<' => {
@@ -100,19 +149,6 @@ impl<'a> Lexer<'a> {
             '-' => Some(Token::Minus),
             '*' => Some(Token::Multiply),
             '/' => Some(Token::Divide),
-            '\"' => {
-                let mut string = String::new();
-                while let Some(next_char) = self.chars.clone().next() {
-                    if next_char != '\"' {
-                        string.push(next_char);
-                        self.chars.next();
-                    } else {
-                        self.chars.next();
-                        break;
-                    }
-                }
-                Some(Token::String(string))
-            }
             '0'..='9' => {
                 let mut number = next_char.to_digit(10)? as i32;
                 while let Some(next_char) = self.chars.clone().next() {
@@ -137,27 +173,6 @@ impl<'a> Lexer<'a> {
                     }
                 }
                 match identifier.as_str() {
-                    /*
-                    TODO:
-                    1. sometimes, unknown tokens cause the rest of the program to not be recognized, outputting []
-                    also make it so that unknown tokens are printed as themselves with Identifier
-                    EX: a < prints [] instead of [Identifier("a"), LessThan]
-                    < DISPLAY aaa prints [LessThan, DISPLAY] instead of [LessThan, DISPLAY, Identifier("aaa")]
-                    a < prints [] instead of [Identifier("a"), LessThan]
-                    IMPORTANT: If the token is not recognized, it should only be an identifier if before/after assignment, in a parameter, or in a list, this logic should be in the parser.
-
-                    2. Also, strings are not recognized, anything in double quotes should be a string
-
-                    3. Parameters DISPLAY("hi") should work by having tokens in between parentheses, so DISPLAY("hi") would be [DISPLAY(String("hi"))]
-                    Current it prints [Display, Unknown, Unknown, Identifier("hi"), Unknown, Unknown] instead of [Display(String("hi"))]
-
-                    4. List lexing: Ex: ["1", 1, 0.1, true] should be [ListCreate(String("1"), Integer(1), Float(0.1), Boolean(true))]
-
-                    5. Comment lexing: Ex: COMMENT asdasd should be [Comment], and anything after on the same line should be ignored.
-                    For comment blocks, remove everything in between the commentblock types.
-
-                    6. Raw strings r"asd", and multiline strings with """asd""" along with formatted strings f"asd {var}".
-                    */
                     "DISPLAY" => Some(Token::Display),
                     "INPUT" => Some(Token::Input),
                     "MOD" => Some(Token::Modulo),
